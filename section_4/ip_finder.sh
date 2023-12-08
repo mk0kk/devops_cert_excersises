2a705d5349468050c04db0e6b82c3457165a823d
mk0kk <81340289+mk0kk@users.noreply.github.com>
2023-12-08 18:31:20 +0200

    Add error message. Change function/variable names

--- section_4/ip_finder.sh
+++ section_4/ip_finder.sh
@@ -8,16 +8,21 @@ INPUT_FILEPATH=$2
 OUTPUT_FLAG=$3
 OUTPUT_FILEPATH=$4
 
-INPUT_FLAG_IS_CORRECT=false
-OUTPUT_FLAG_IS_CORRECT=false
+INPUT_FLAG_RAISED=false
+OUTPUT_FLAG_RAISED=false
+HELP_FLAG_RAISED=false
+
+RED='\033[0;31m'
+NC='\033[0m'
 
 main() {
     check_input_flag "$INPUT_FLAG"
     check_output_flag "$OUTPUT_FLAG"
 
-    if [ "$INPUT_FLAG_IS_CORRECT" == false ] || [ "$OUTPUT_FLAG_IS_CORRECT" == false ] || [ "$HELP_FLAG_IS_CORRECT" == true ]; then
+    if [ "$INPUT_FLAG_RAISED" == false ] || [ "$OUTPUT_FLAG_RAISED" == false ]; then
+        echo -e "${RED}ERROR: Input or output options are missing/wrong ${NC}.\nRefer to documentation by providing the "-h" or "--help" option:\n\nuser@computer:~$ ./ip-finder.sh --help\n"
+    elif [ "$HELP_FLAG_RAISED" == true ]; then
         less readme.txt
-        #echo -e "\n"
     else
         find_and_export_ip_addr "$IP_ADDR_EXPRESSION" "$INPUT_FILEPATH" "$OUTPUT_FILEPATH"
     fi
@@ -29,25 +34,25 @@ find_and_export_ip_addr(){
 
 check_help_flag(){
     if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
-        HELP_FLAG_IS_CORRECT=true
+        HELP_FLAG_RAISED=true
     else
-        HELP_FLAG_IS_CORRECT=false
+        HELP_FLAG_RAISED=false
     fi    
 }
 
 check_input_flag(){
     if [ "$1" == "--input-file" ] || [ "$1" == "-i" ]; then
-        INPUT_FLAG_IS_CORRECT=true
+        INPUT_FLAG_RAISED=true
     else
-        INPUT_FLAG_IS_CORRECT=false
+        INPUT_FLAG_RAISED=false
     fi
 }
 
 check_output_flag(){
     if [ "$1" == "--output-file" ] || [ "$1" == "-o" ]; then
-        OUTPUT_FLAG_IS_CORRECT=true
+        OUTPUT_FLAG_RAISED=true
     else
-        OUTPUT_FLAG_IS_CORRECT=false
+        OUTPUT_FLAG_RAISED=false
     fi
 }
 
